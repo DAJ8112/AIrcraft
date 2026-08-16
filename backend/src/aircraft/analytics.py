a9423da2e1176236a821cd758058b17175c2097f
DAJ8112 <daj8112@gmail.com>
2026-08-16 13:24:37 -0400

    init backend

---
+++ backend/src/aircraft/analytics.py
@@ -0,0 +1,3 @@
+"""
+Parse an Excel file (Aircraft metrics logs) -> analyzes a record ->  returns statistical analysis as a context for llm (as structured json context) 
+"""
---
+++ backend/src/aircraft/maintenance_analyzer.py
@@ -0,0 +1,3 @@
+"""
+Sends the json (form analytics module) + other context (aircraft manual) to LLM (AWS Bedrock) -> returns a structured maintenance report (json)
+"""
